07a87699413d260a267d80cd45731a018f9dd934
nasa <2974464448@qq.com>
2023-05-21 20:04:30 +0800

    100003

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,6 @@ const routes = [
             {
                 path: "/home", 
                 redirect: "/home/film",
-                
             },
             {
                 path: "/home/film", 
